cc911152436280627071b232e90fd6e45d52d1fc
Rodrigo2901 <u202215489@upc.edu.pe>
2024-06-05 20:03:37 -0500

    chore(iam): revert commit.

--- Planifi-backend/obj/Debug/net8.0/Planifi-backend.AssemblyInfo.cs
+++ Planifi-backend/obj/Debug/net8.0/Planifi-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Planifi-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20a9843ea12ceecc81fb984a6a6fe44fe1f4ff2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+337255999219d88f90acfc7f0b76467ae7af38da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Planifi-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Planifi-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
