46fcb2a2b2e11ae2f3b7554e6a56d05b9d1b1cef
Rodrigo2901 <u202215489@upc.edu.pe>
2024-06-05 19:20:25 -0500

    feat(profile): added Infrastructure layer of Profiles bounded context.

---
+++ Planifi-backend/Profiles/Infrastructure/Persistence/EFC/Repositories/ProfileRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Planifi_backend.IAM.Domain.Model.Aggregates;
+using Planifi_backend.IAM.Domain.Model.ValueObjects;
+using Planifi_backend.Profiles.Domain.Repositories;
+using Planifi_backend.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Planifi_backend.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace Planifi_backend.Profiles.Infrastructure.Persistence.EFC.Repositories;
+
+public class ProfileRepository(AppDbContext context) : BaseRepository<Profile>(context), IProfileRepository
+{
+    public Task<Profile?> FindProfileByEmailAsync(EmailAddress email)
+    {
+        return Context.Set<Profile>().Where(p => p.Email == email).FirstOrDefaultAsync();
+    }
+}
