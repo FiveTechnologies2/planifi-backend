713664bce0260b71c75b778aa850ecd6ba4debdf
Karito Dianeth Medina Chocce <108374869+Karito07@users.noreply.github.com>
2024-06-07 21:17:15 -0500

    feature(worker): Added WorkerAudit

---
+++ Planifi-backend/Workers/Domain/Model/Aggregates/WorkerAudit.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using EntityFrameworkCore.CreateUpdateDate.Contracts;
+
+namespace Planifi_backend.Workers.Domain.Model.Aggregates;
+
+public partial class Worker : IEntityWithCreateUpdateDate
+{
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    [Column("UpdateAt")] public DateTimeOffset? UpdatedDate { get; set; }
+}
