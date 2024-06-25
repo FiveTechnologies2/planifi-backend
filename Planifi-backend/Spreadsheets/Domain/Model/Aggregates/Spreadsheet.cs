2bb1e71509182b3785cd96d81510055805608e89
Salvlormonch1 <salvlormonch@gmail.com>
2024-06-25 02:47:40 -0500

    feat: Spreadsheet entity corrected

--- Planifi-backend/Spreadsheets/Domain/Model/Aggregates/Spreadsheet.cs
+++ Planifi-backend/Spreadsheets/Domain/Model/Aggregates/Spreadsheet.cs
@@ -1,5 +1,6 @@
 ﻿using Planifi_backend.Spreadsheets.Domain.Model.Commands;
 using Planifi_backend.Spreadsheets.Domain.Model.ValueObjects;
+using Planifi_backend.Workers.Domain.Model.Aggregates;
 
 namespace Planifi_backend.Spreadsheets.Domain.Model.Aggregates;
 
@@ -12,12 +13,15 @@ public partial class Spreadsheet
     public string SpreadsheetDescription { get; private set; }
     
     public CompanyId CompanyId { get; set; }
+    
+    public List<Worker> Workers { get; set; }
 
     public Spreadsheet(string name, string description, CompanyId companyId)
     {
         SpreadsheetName = name;
         SpreadsheetDescription = description;
         CompanyId = companyId;
+        Workers = new List<Worker>();
     }
 
     public Spreadsheet(CreateSpreadsheetCommand command)
@@ -25,5 +29,6 @@ public partial class Spreadsheet
         SpreadsheetName = command.Name;
         SpreadsheetDescription = command.Description;
         CompanyId = new CompanyId(command.CompanyId);
+        Workers = new List<Worker>();
     }
 }
