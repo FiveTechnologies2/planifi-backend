fa4e939905b78f6574c5b42ca3944c94c055f8e0
✨ KARITO ✨ <108374869+Karito07@users.noreply.github.com>
2024-06-24 21:57:08 -0500

    feature(Workers): Added Queries

---
+++ Planifi-backend/Workers/Domain/Model/Queries/GetWorkerByEmailQuery.cs
@@ -0,0 +1,14 @@
+﻿using Planifi_backend.Workers.Domain.Model.ValueObjects;
+
+namespace Planifi_backend.Workers.Domain.Model.Queries
+{
+    public class GetWorkerByEmailQuery
+    {
+        public EmailAddress Email { get; set; }
+
+        public GetWorkerByEmailQuery(EmailAddress email)
+        {
+            Email = email;
+        }
+    }
+}
