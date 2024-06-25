425a023fd89983f5ad66399505e37ad93ef55252
✨ KARITO ✨ <108374869+Karito07@users.noreply.github.com>
2024-06-24 22:05:09 -0500

    feature(Workers): Added IWorkerContextFacade.cs

---
+++ Planifi-backend/Workers/Interfaces/ACL/IWorkerContextFacade.cs
@@ -0,0 +1,8 @@
+﻿namespace Planifi_backend.Workers.Interfaces.ACL;
+
+public interface IWorkersContextFacade
+{
+    Task<int> CreateWorker(string firstName, string lastName, string email, string phone, string address, string position, string workedHours, string extraHours, string performance);
+
+    Task<int> FetchWorkerIdByEmail(string email);
+}
