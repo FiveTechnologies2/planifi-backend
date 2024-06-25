345598fed4d9330c7cd36846e95285dc9c368bdc
✨ KARITO ✨ <108374869+Karito07@users.noreply.github.com>
2024-06-24 21:59:04 -0500

    feature(Workers): Added Worker Value Objects

---
+++ Planifi-backend/Workers/Domain/Model/ValueObjects/WorkerInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace Planifi_backend.Workers.Domain.Model.ValueObjects;
+
+public class WorkerInfo
+{
+    public string Position { get; private set; }
+    public string WorkedHours { get; private set; }
+    public string ExtraHours { get; private set; }
+    public string Performance { get; private set; }
+    
+    public WorkerInfo() { }
+    
+    public WorkerInfo(string position, string workedHours, string extraHours, string performance)
+    {
+        Position = position;
+        WorkedHours = workedHours;
+        ExtraHours = extraHours;
+        Performance = performance;
+    }
+}
---
+++ Planifi-backend/Workers/Domain/Model/ValueObjects/WorkerName.cs
@@ -0,0 +1,21 @@
+﻿namespace Planifi_backend.Workers.Domain.Model.ValueObjects;
+
+public class WorkerName
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+
+    public WorkerName()
+    {
+        FirstName = string.Empty;
+        LastName = string.Empty;
+    }
+    
+    public WorkerName(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+    
+    public string FullName => $"{FirstName} {LastName}";
+}
